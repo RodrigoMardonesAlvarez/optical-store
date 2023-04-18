88fb7f189b00d50d0f752d3c354505565a42f28b
RodrigoMardonesAlvarez <rodrigomardones@protonmail.com>
2023-04-17 22:01:49 -0400

    Fetching onClick events

--- Optical/js/script.js
+++ Optical/js/script.js
@@ -2,19 +2,19 @@
 function myAccFunc() {
     var x = document.getElementById("demoAcc");
     if (x.className.indexOf("show") == -1) {
-      x.className += "show";
+      x.className += " show";
     } else {
       x.className = x.className.replace(" show", "");
     }
   }
-  // Click to open the accordion
+  // Click on the "Jeans" link on page load to open the accordion for demo purposes
   document.getElementById("myBtn").click();
   // Open and close sidebar
-  function open() {
+  function _open() {
     document.getElementById("mySidebar").style.display = "block";
     document.getElementById("myOverlay").style.display = "block";
   }
-  function close() {
+  function _close() {
     document.getElementById("mySidebar").style.display = "none";
     document.getElementById("myOverlay").style.display = "none";
   }
